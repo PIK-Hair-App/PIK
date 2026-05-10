fef7920857426771602dfaecea6526471ec56050
Sarah <sarahnurse2006@gmail.com>
2026-05-10 16:29:55 -0400

    Initialized project repo

---
+++ COL-hair/src/components/themed-view.jsx
@@ -0,0 +1,16 @@
+import { View, ViewProps } from 'react-native';
+
+import { ThemeColor } from '@/constants/theme';
+import { useTheme } from '@/hooks/use-theme';
+
+ThemedViewProps = ViewProps & {
+  lightColor: string,
+  darkColor: string,
+  type: ThemeColor,
+};
+
+export function ThemedView({ style, lightColor, darkColor, type, ...otherProps }: ThemedViewProps) {
+  const theme = useTheme();
+
+  return <View style={[{ backgroundColor: theme[type ?? 'background'] }, style]} {...otherProps} />;
+}
